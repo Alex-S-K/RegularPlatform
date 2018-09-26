a3d420719d9f197fff3b3e58007d573135a38844
Alex <Alex@AlexdeMacBook-Pro.local>
2018-09-26 16:33:28 +0800

    Init Project

---
+++ src/main/java/com/alex/RegularPlatform/RegularPlatformApplication.java
@@ -0,0 +1,12 @@
+package com.alex.RegularPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegularPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegularPlatformApplication.class, args);
+	}
+}
